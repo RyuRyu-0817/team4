2309de34b4ef8904770a5fe9d63c6dad56867a09
m_oka <pai92zcl@s.okayama-u.ac.jp>
2024-09-26 11:43:09 +0000

    knowhow_views.pyのindexを作成

--- team4_app/urls.py
+++ team4_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('home/', home_views.index, name='home_index'),  # home
     
     # ノウハウのルーティングここから
-    # path('knowhow/', knowhow_views.index, name='knowhow_index'),  # knowhow
+    path('knowhow/', knowhow_views.index, name='knowhow_index'),  # knowhow
     # path('knowhow/<int:id>/', knowhow_views.???)
     # path('knowhow/create/', knowhow_views.???)
 
